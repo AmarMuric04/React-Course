3f2670895233aa5a93d70b635e3e4e80aaa47fcc
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-03-12 02:36:21 +0100

    Learned more about JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,73 @@
+import mainImage from "./assets/react-core-concepts.png";
+import componentsImage from "./assets/components.png";
+
+const reactDescriptions = ["boss", "npc", "lider"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const licnost = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={mainImage} alt="Stylized atom" />
+      <h1>Murgin React Website</h1>
+      <p>
+        Sve sto treba da znate jeste da sam ja {licnost} i nemojte da mnjaucete
+        laga
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+function Main() {
+  return (
+    <main>
+      <section id="core-concepts">
+        <h2>Vreme je da pocnemo!</h2>
+        <ul>
+          <CoreConcept
+            title="Concepts"
+            description="Counter Strike 2"
+            image={componentsImage}
+          />
+          <CoreConcept
+            title="Ferrari"
+            description="Maximillian Robespierre"
+            image={componentsImage}
+          />
+          <CoreConcept
+            title="Lamborghini"
+            description="Badass dog"
+            image={componentsImage}
+          />
+          <CoreConcept
+            title="Ford "
+            description="Mustang goes wroom"
+            image={componentsImage}
+          />
+        </ul>
+      </section>
+    </main>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+      <Header />
+      <Main />
     </div>
   );
 }
