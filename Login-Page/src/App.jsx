3e64b9172d19caaf52d604b27731f0537a23cca6
AmarMuric04 <muricamar2004@gmail.com>
2024-03-18 17:20:58 +0100

    More practice with refs, & learned useImperativeHandle

--- Login-Page/src/App.jsx
+++ Login-Page/src/App.jsx
@@ -1,20 +1,16 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-import SignUp from "../components/SignUp";
-import SignIn from "../components/Signin";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/Signin";
 
 function App() {
   const [showPage, setShowPage] = useState("signin");
 
-  function handleSignUp() {
-    setShowPage("signup");
-  }
-
-  function handleSignIn() {
-    setShowPage("signin");
+  function handleShowPage(identiifier) {
+    identiifier === "signup" ? setShowPage("signup") : setShowPage("signin");
   }
 
   return (
@@ -23,10 +19,18 @@ function App() {
         {showPage === "signup" ? <SignUp /> : <SignIn />}
 
         <section id="buttons-section">
-          <Button onClick={handleSignUp} id="signup-button" variant="text">
+          <Button
+            onClick={handleShowPage.bind(null, "signup")}
+            id="signup-button"
+            variant="text"
+          >
             Sign up
           </Button>
-          <Button onClick={handleSignIn} id="signin-button" variant="contained">
+          <Button
+            onClick={handleShowPage.bind(null, "signin")}
+            id="signin-button"
+            variant="contained"
+          >
             Sign in
           </Button>
         </section>
--- Sixth-Section-Course/src/components/TimerChallenge.jsx
+++ Sixth-Section-Course/src/components/TimerChallenge.jsx
@@ -3,40 +3,53 @@ import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
-  const timer = useRef();
+  const interval = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(interval.current);
+    dialog.current.openModal();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.showModal();
-    }, targetTime * 1000);
-
-    setTimerStarted(true);
+    interval.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    clearInterval(interval.current);
+    dialog.current.openModal();
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="Lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : null}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : null}>
-          {timerStarted ? "Timer is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : null}>
+          {timerIsActive ? "Timer is running..." : "Timer inactive"}
         </p>
       </section>
     </>
