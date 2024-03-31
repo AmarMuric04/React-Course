830154660b6f89e2c1fbb57134d142cc88a2795b
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-03-31 19:29:23 +0200

    Some visual work on the quiz

--- Tenth-Section-Course/src/App.jsx
+++ Tenth-Section-Course/src/App.jsx
@@ -1,3 +1,31 @@
-function App() {}
+function App() {
+  return (
+    <>
+      <main id="quiz">
+        <div id="question">
+          <progress value={100} max={150} />
+          <h1 id="question-overview">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+            harum.
+          </h1>
+        </div>
+        <menu id="answers">
+          <div className="answer">
+            <button>Lorem ipsum dolor sit amet.</button>
+          </div>
+          <div className="answer">
+            <button className="selected">Lorem ipsum dolor sit amet.</button>
+          </div>
+          <div className="answer">
+            <button>Lorem ipsum dolor sit amet.</button>
+          </div>
+          <div className="answer">
+            <button>Lorem ipsum dolor sit amet.</button>
+          </div>
+        </menu>
+      </main>
+    </>
+  );
+}
 
 export default App;
