fbaa9bc8cc6696f118d78d9c4b089bf70834ff38
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-04-01 02:00:59 +0200

    More work on solo Quiz

--- Tenth-Section-Course/src/App.jsx
+++ Tenth-Section-Course/src/App.jsx
@@ -1,26 +1,53 @@
+import Logo from "./assets/quiz-logo.png";
+
+import { useState } from "react";
+
 function App() {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [checkAnswer, setCheckAnswer] = useState("");
+  const [buttonClass, setButtonClass] = useState("");
+
+  function handleSelect(identifier) {
+    setSelectedAnswer(identifier);
+    setButtonClass("selected");
+
+    setTimeout(() => {
+      setCheckAnswer(identifier);
+      setButtonClass("correct");
+    }, 1000);
+  }
   return (
     <>
+      <header>
+        <img src={Logo} alt="" />
+        <h1>REACTQUIZ</h1>
+      </header>
       <main id="quiz">
         <div id="question">
           <progress value={100} max={150} />
-          <h1 id="question-overview">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
-            harum.
-          </h1>
+
+          <h2>How do you typically render list content in React apps?</h2>
         </div>
         <menu id="answers">
-          <div className="answer">
-            <button>Lorem ipsum dolor sit amet.</button>
+          <div onClick={handleSelect.bind(null, "first")} className="answer">
+            <button className={selectedAnswer === "first" ? buttonClass : ""}>
+              Lorem ipsum dolor sit amet.
+            </button>
           </div>
-          <div className="answer">
-            <button className="selected">Lorem ipsum dolor sit amet.</button>
+          <div onClick={handleSelect.bind(null, "second")} className="answer">
+            <button className={selectedAnswer === "second" ? buttonClass : ""}>
+              Lorem ipsum dolor sit amet.
+            </button>
           </div>
-          <div className="answer">
-            <button>Lorem ipsum dolor sit amet.</button>
+          <div onClick={handleSelect.bind(null, "third")} className="answer">
+            <button className={selectedAnswer === "third" ? buttonClass : ""}>
+              Lorem ipsum dolor sit amet.
+            </button>
           </div>
-          <div className="answer">
-            <button>Lorem ipsum dolor sit amet.</button>
+          <div onClick={handleSelect.bind(null, "fourth")} className="answer">
+            <button className={selectedAnswer === "fourth" ? buttonClass : ""}>
+              Lorem ipsum dolor sit amet.
+            </button>
           </div>
         </menu>
       </main>
