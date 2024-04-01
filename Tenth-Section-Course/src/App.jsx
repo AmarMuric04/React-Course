27ee1449a1708c644e6bb1e1af276c78f2110f5e
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-04-02 00:34:56 +0200

    More work with the Quiz

--- Tenth-Section-Course/src/App.jsx
+++ Tenth-Section-Course/src/App.jsx
@@ -1,4 +1,5 @@
 import Logo from "./assets/quiz-logo.png";
+import QUESTIONS from "./questions.js";
 
 import { useState } from "react";
 
@@ -6,6 +7,30 @@ function App() {
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const [checkAnswer, setCheckAnswer] = useState("");
   const [buttonClass, setButtonClass] = useState("");
+  const [question, setQuestion] = useState(
+    <>
+      <div id="question">
+        <progress value={100} max={150} />
+
+        <h2>{QUESTIONS[0].text}</h2>
+      </div>
+      <menu id="answers">
+        {QUESTIONS[0].answers.map((answer, index) => (
+          <div
+            key={index}
+            onClick={handleSelect.bind(null, "first")}
+            className="answer"
+          >
+            <button className={selectedAnswer === "first" ? buttonClass : ""}>
+              {answer}
+            </button>
+          </div>
+        ))}
+      </menu>
+    </>
+  );
+
+  let questionIndex = 0;
 
   function handleSelect(identifier) {
     setSelectedAnswer(identifier);
@@ -15,6 +40,39 @@ function App() {
       setCheckAnswer(identifier);
       setButtonClass("correct");
     }, 1000);
+
+    if (questionIndex === QUESTIONS.length - 1) return;
+
+    setTimeout(() => {
+      handleShowQuestions(questionIndex);
+    }, 2000);
+  }
+
+  function handleShowQuestions(index) {
+    setQuestion(
+      <>
+        <div id="question">
+          <progress value={100} max={150} />
+
+          <h2>{QUESTIONS[index].text}</h2>
+        </div>
+        <menu id="answers">
+          {QUESTIONS[index].answers.map((answer, i) => (
+            <div
+              key={i}
+              onClick={handleSelect.bind(null, i)}
+              className="answer"
+            >
+              <button className={selectedAnswer === i ? buttonClass : ""}>
+                {answer}
+              </button>
+            </div>
+          ))}
+        </menu>
+      </>
+    );
+
+    questionIndex++;
   }
   return (
     <>
@@ -22,35 +80,7 @@ function App() {
         <img src={Logo} alt="" />
         <h1>REACTQUIZ</h1>
       </header>
-      <main id="quiz">
-        <div id="question">
-          <progress value={100} max={150} />
-
-          <h2>How do you typically render list content in React apps?</h2>
-        </div>
-        <menu id="answers">
-          <div onClick={handleSelect.bind(null, "first")} className="answer">
-            <button className={selectedAnswer === "first" ? buttonClass : ""}>
-              Lorem ipsum dolor sit amet.
-            </button>
-          </div>
-          <div onClick={handleSelect.bind(null, "second")} className="answer">
-            <button className={selectedAnswer === "second" ? buttonClass : ""}>
-              Lorem ipsum dolor sit amet.
-            </button>
-          </div>
-          <div onClick={handleSelect.bind(null, "third")} className="answer">
-            <button className={selectedAnswer === "third" ? buttonClass : ""}>
-              Lorem ipsum dolor sit amet.
-            </button>
-          </div>
-          <div onClick={handleSelect.bind(null, "fourth")} className="answer">
-            <button className={selectedAnswer === "fourth" ? buttonClass : ""}>
-              Lorem ipsum dolor sit amet.
-            </button>
-          </div>
-        </menu>
-      </main>
+      <main id="quiz">{question}</main>
     </>
   );
 }
