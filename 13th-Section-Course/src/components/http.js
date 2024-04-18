ecec3b291aa71e293b8bad886a0a922054062a3d
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-04-18 21:42:00 +0200

    Learning how to put things inside a database and how to remove it

---
+++ 13th-Section-Course/src/components/http.js
@@ -0,0 +1,24 @@
+export async function fetchAvailablePlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const data = await response.json();
+
+  if (!response.ok) throw new Error("Something went wrong <3");
+
+  return data.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) throw new Error("Something went wrong <3");
+
+  return resData.message;
+}
