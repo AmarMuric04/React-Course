2bee749c27559dcad1ed22e7200231368739c948
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-04-26 03:41:37 +0200

    Fixed visuals

--- Knowledge/src/components/Modal.jsx
+++ Knowledge/src/components/Modal.jsx
@@ -3,16 +3,14 @@ import {
   useRef,
   useImperativeHandle,
   useContext,
-  useEffect,
   useState,
 } from "react";
 
 import ModalContext from "../store/modal-context";
 
-let modalOpen = false;
-
 const Modal = forwardRef(function Modal(props, ref) {
   const modal = useRef();
+  const timer = useRef();
   const { close } = useContext(ModalContext);
   const [isOpen, setIsOpen] = useState();
 
@@ -23,37 +21,44 @@ const Modal = forwardRef(function Modal(props, ref) {
         setIsOpen(true);
       },
       close() {
-        modal.current.close();
         setIsOpen(false);
+        clearTimeout(timer.current);
+
+        timer.current = setTimeout(() => {
+          modal.current.close();
+        }, 1000);
       },
     };
   });
 
   return (
     <dialog
+      className={`bg-white max-w-3xl p-10 shadow-2xl md overflow-auto transition-all duration-500  ${
+        isOpen ? "translate-y-0 opacity-1" : "translate-y-32 opacity-0"
+      }`}
       ref={modal}
       id="modal"
-      className={`bg-white p-10 flex-col gap-3 transition-all duration-500 overflow-auto md max-w-3xl shadow-2xl ${
-        isOpen ? "flex translate-y-0" : "translate-y-24"
-      }`}
     >
-      <p
-        onClick={close}
-        className="absolute uppercase right-3 top-2 text-1xl cursor-pointer px-2 flex justify-center items-center hover:bg-stone-900 hover:text-white transition-all duration-200"
-      >
-        x
-      </p>
-      <h1 className="text-center border-t-2 border-gray-900 max-w-sm font-bold uppercase text-5xl font-mono mb-5">
-        Hello there :D
-      </h1>
-      <p className="max-w-xl">
-        This is just a place where I'll be testing some of the things I've
-        already learned previously in the well known Maximillian's React Course.
-      </p>
-      <p className="max-w-xl">
-        Here I am learning/revisiting how to use Tailwind with React and how to
-        use context and ForwardRefs together with useImperativeHandle.
-      </p>
+      <div className="bg-white flex flex-col gap-4 h-full">
+        <p
+          onClick={close}
+          className="absolute uppercase right-3 top-2 text-1xl cursor-pointer px-2 flex justify-center items-center hover:bg-stone-900 hover:text-white transition-all duration-200"
+        >
+          x
+        </p>
+        <h1 className="text-center border-t-2 border-gray-900 max-w-sm font-bold uppercase text-5xl font-mono mb-5">
+          Hello there :D
+        </h1>
+        <p className="max-w-xl">
+          This is just a place where I'll be testing some of the things I've
+          already learned previously in the well known Maximillian's React
+          Course.
+        </p>
+        <p className="max-w-xl">
+          Here I am learning/revisiting how to use Tailwind with React and how
+          to use context and ForwardRefs together with useImperativeHandle.
+        </p>
+      </div>
     </dialog>
   );
 });
