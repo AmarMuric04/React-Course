b41f8124fe60c05d0964ff1c74ef2fddef98e8e2
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-05-21 23:15:45 +0200

    sending data to firebase

--- 17th/src/App.js
+++ 17th/src/App.js
@@ -1,8 +1,23 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const cart = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    fetch(
+      "https://console.firebase.google.com/project/murga-eca7b/database/murga-eca7b-default-rtdb/data/~2F/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       <Cart />
