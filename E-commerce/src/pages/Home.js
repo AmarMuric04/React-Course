1a64c68d7519a86a67782b14b5deaef62df079f4
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-06-02 20:43:47 +0200

    Created half of home page

--- E-commerce/src/pages/Home.js
+++ E-commerce/src/pages/Home.js
@@ -1,3 +1,21 @@
+import image from "../assets/homeimg.jpeg";
+
 export default function HomePage() {
-  return <h1 className="text-black">Home page :D</h1>;
+  return (
+    <main className="w-full h-screen poppins">
+      <div className="h-screen w-full flex justify-around align-center">
+        <section className="w-1/2 flex flex-col my-16 px-16">
+          <h1 className="tracking-[0.2rem] text-4xl font-bold text-green-400">
+            E-commerce
+          </h1>
+          <p className="text-zinc-200 text-md foont-bold">
+            Buy whatever you want
+          </p>
+        </section>
+        <section className="w-1/2 h-full flex items-center justify-center">
+          <img className="w-1/2 h-1/2 object-contain" src={image} />
+        </section>
+      </div>
+    </main>
+  );
 }
--- E-commerce/src/pages/Shop.js
+++ E-commerce/src/pages/Shop.js
@@ -1,11 +1,18 @@
-import { Await, useRouteLoaderData } from "react-router-dom";
+import {
+  Await,
+  useRouteLoaderData,
+  useActionData,
+  Link,
+} from "react-router-dom";
 import Products from "../components/Products";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { Suspense } from "react";
 
 export default function ShopPage() {
   const { products } = useRouteLoaderData("shoproot");
+
+  const [page, setPage] = useState(1);
 
   return (
     <Fragment>
@@ -13,7 +20,7 @@ export default function ShopPage() {
       <main className="w-[85%] px-16 pt-16">
         <h1
           className="uppercase monsterrat tracking-[0.1rem] font-thin text-4xl
-       mb-16 text-green-400"
+       mb-16 text-green-400 flex flex-col"
         >
           Available products
         </h1>
@@ -22,11 +29,13 @@ export default function ShopPage() {
             {(loadedData) => <Products products={loadedData} />}
           </Await>
         </Suspense>
-        <button>+</button>
+        <Link to={`page=${1}`}>+21312312312321</Link>
       </main>
     </Fragment>
   );
 }
+
+export const action = async (request, params) => {};
 
 export const loader = async () => {
   return fetch("https://dummyjson.com/products?limit=30&skip=60");
