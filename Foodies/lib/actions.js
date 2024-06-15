ab582b4d302f58ee6d3fa0caff4a227d8f2127f9
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-06-16 01:00:36 +0200

    Form validation using NextJS and some react-dom hooks

--- Foodies/lib/actions.js
+++ Foodies/lib/actions.js
@@ -3,7 +3,11 @@
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text.title || text.title.trim() === "";
+}
+
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get("title"),
     instructions: formData.get("instructions"),
@@ -13,6 +17,18 @@ export async function shareMeal(formData) {
     creator_email: formData.get("email"),
   };
 
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(title.creator) ||
+    isInvalidText(title.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return { message: "invalid input." };
+  }
   await saveMeal(meal);
   redirect("/meals");
 }
