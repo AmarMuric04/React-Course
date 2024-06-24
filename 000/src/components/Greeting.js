f2985e1b4b233e565e442e65d6a48898a2c39437
AmarMuric04 <muricamar2004@gmail.com>
2024-06-24 19:48:02 +0200

    Finished with the introduction to unit testing

--- 000/src/components/Greeting.js
+++ 000/src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import Output from "./Output";
+
 export default function Greeting() {
+  const [changedText, setChangedText] = useState();
+
+  const handleChangeText = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello world!</h2>
-      <p>It's good to see u</p>
+      {!changedText && <Output>It's good to see u</Output>}
+      {changedText && <Output>Changed!</Output>}
+      <button onClick={handleChangeText}>Change text!</button>
     </div>
   );
 }
--- 000/src/components/Greeting.test.js
+++ 000/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Greeting component", () => {
   test("successfully displaying Hello World!", () => {
@@ -12,5 +13,36 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText(/hello world/i);
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders greeting text", () => {
+    render(<Greeting />);
+
+    const greetingTextElement = screen.getByText(/good to see/i, {
+      exact: false,
+    });
+    expect(greetingTextElement).toBeInTheDocument();
+  });
+
+  test("renders changed if the button was clicked", () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const changedTextElement = screen.getByText("Changed!");
+    expect(changedTextElement).toBeInTheDocument();
+  });
+
+  test("doesn't render original text when button is clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByText("Change text!");
+    userEvent.click(buttonElement);
+
+    const originalText = screen.queryByText(/good to see/i, { exact: false });
+    expect(originalText).toBeNull();
   });
 });
