8ebed755408a0f5a8f04c73397733f78791df918
AmarMuric04 <muricamar2004@gmail.com>
2024-06-25 20:45:59 +0200

    typescript types

---
+++ typescript/basics.ts
@@ -0,0 +1,34 @@
+let age: number;
+
+age = 12;
+
+let userName: string = "amar";
+
+let isInstructor: boolean;
+
+isInstructor = false;
+
+let hobbies: string[];
+
+hobbies = ["this one"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Amar",
+  age: 19,
+};
+
+let groupOfPeople: {
+  name: string;
+  age: number;
+}[];
+
+// type inference !
+
+const firstName = "amar";
+
+firstName = 19;
