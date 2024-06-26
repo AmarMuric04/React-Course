784b0e1fe9b02d4e0754fd431331a62c5bbc33fa
AmarMuric04 <muricamar2004@gmail.com>
2024-06-26 15:57:31 +0200

    More work with React and typescript

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -12,6 +12,18 @@ let hobbies: string[];
 
 hobbies = ["this one"];
 
+type Person = {
+  name: string;
+  age: number;
+};
+
+let personPerson: Person;
+
+personPerson = {
+  name: "amar",
+  age: 12,
+};
+
 let person: {
   name: string;
   age: number;
@@ -31,4 +43,25 @@ let groupOfPeople: {
 
 const firstName = "amar";
 
-firstName = 19;
+// firstName = 19; wrong!!!
+
+let id: number | string = 123;
+
+id = "amar";
+
+// functions & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function printOutput(value: any) {
+  console.log(value);
+}
+
+// generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
